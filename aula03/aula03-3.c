5e0bbb874a119417bddc343cb7d211e107c07dfb
Danilo Abreu <daniloabreu1@gmail.com>
2018-08-08 12:08:12 -0300

    aula03-3-4

---
+++ aula03/aula03-3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+main(){
+
+    float c;
+    printf("DIGITE A TEMPERATURA EM CELSIUS ");
+    scanf("%f", &c);
+    printf("%.2f",((c*1.8)+32));
+}
---
+++ aula03/aula03-4.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+main(){
+    float m;
+    printf("DIGITE A DISTANCIA EM m/s ");
+    scanf("%f", &m);
+    printf("EM k/h %.2f",m*3.6);
+}
