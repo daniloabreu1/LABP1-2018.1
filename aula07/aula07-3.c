9ccb0070769a84648ee876beb2d43232c221997a
Danilo Abreu <daniloabreu1@gmail.com>
2018-08-24 10:11:46 -0300

    aula07-3

---
+++ aula07/aula07-3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <windows.h>
+main(){
+    int min,max,i;
+    printf("min: ");
+    scanf("%d",&min);
+    printf("max: ");
+    scanf("%d",&max);
+    system("cls");
+    if(min>max){
+        int a=min;
+        min=max;
+        max=a;
+    }
+    for(;min<=max;min++){
+        for(i=0;i<=10;i++){
+                printf("%02d x %02d = %02d\n",min,i,min*i);
+        }
+        putchar('\n');
+    }
+}
